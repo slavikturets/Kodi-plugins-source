8fdab610173bb3514770dc1d7ac8a45198408b28
learningit <scottyroscoe13@gmail.com>
2016-06-26 17:33:51 -0400

    [plugin.video.abcfamily] 3.0.2

---
+++ plugin.video.abcfamily/readme.txt
@@ -0,0 +1,8 @@
+plugin.video.t1m.abcfamily
+================
+
+Kodi Addon for ABC Family website
+
+V3.0.1 separate scraper for future functions
+V2.0.2 cleanup for release
+V2.0.1 Initial version
--- plugin.video.abcfamily/resources/lib/scraper.py
+++ plugin.video.abcfamily/resources/lib/scraper.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
-# WABC Kodi Video Addon
+# ABCFamily Kodi Video Addon
 #
 from t1mlib import t1mAddon
+import json
 import re
+import os
+import datetime
 import urllib
 import xbmc
 import xbmcplugin
 import xbmcgui
 import HTMLParser
 import sys
-import os
 
 h = HTMLParser.HTMLParser()
 qp  = urllib.quote_plus
@@ -20,32 +22,27 @@ UTF8     = 'utf-8'
 class myAddon(t1mAddon):
 
   def getAddonMenu(self,url,ilist):
-      ihtml = self.getRequest('http://abc.go.com/shows')
-      html = self.getRequest('http://abc.go.com/shows/abc-updates/news/insider/143-for-free-watch-abc-watch-full-episodes-with-no-sign-in-042715?cid=abchp_143_for_free')
-      html = re.compile('<section class="m-blog_detail-body(.+?)</section', re.DOTALL).search(html).group(1)
-      a = re.compile('<p align="center".+?<a.+?name="(.+?)".+?<a href="(.+?)">(.+?)</a>',re.DOTALL).findall(html)
-      for id, url, name in a:
-          name = name.replace('<strong>','').replace('</strong>','').replace('<b>','').replace('</b>','')
-          name=h.unescape(name.decode(UTF8))
-          thumb = re.compile('<main class="content">.+?<a href="'+url.replace('http://abc.go.com','')+'".+?class="tablet-source".+?srcset="(.+?) ',re.DOTALL).search(ihtml)
-          if thumb is not None:
-              thumb = thumb.group(1)
-              fanart = thumb
-          else:
-              thumb = self.addonIcon
-              fanart = self.addonFanart
+      ihtml = self.getRequest('http://freeform.go.com/shows')
+      html = re.compile('<main(.+?)</main', re.DOTALL).search(ihtml).group(1)
+      a = re.compile('<li.+?<a href="(.+?)".+?class="background-link(.+?)".+?class="tablet-source.+?srcset="(.+?) .+?</li',re.DOTALL).findall(html)
+      for url, bg, thumb in a:
+          if not 'bg-gradient' in bg:
+              continue
+          name = url.rsplit('/',1)[1]
+          name = name.replace('-',' ').title()
+          fanart = thumb
           infoList ={}
           infoList['Title'] = name
           infoList['TVShowTitle'] = name
-          infoList['mediatype'] = 'tvshow'
           contextMenu = [('Add To Library','XBMC.RunPlugin(%s?mode=DF&url=AL%s)' % (sys.argv[0], url))]
           ilist = self.addMenuItem(name,'GE', ilist, url, thumb, fanart, infoList, isFolder=True, cm=contextMenu)
       return(ilist)
 
-
-  def getAddonEpisodes(self,url,ilist, getFileData = False):
+  def getAddonEpisodes(self,url,ilist, getFileData=False):
       self.defaultVidStream['width']  = 1920
       self.defaultVidStream['height'] = 1080
+      if not url.startswith('http'):
+         url = 'http://freeform.go.com'+url
       if not url.endswith('/episode-guide'):
           url = url+'/episode-guide'
       html = self.getRequest(url)
@@ -63,7 +60,7 @@ class myAddon(t1mAddon):
               infoList['Episode'] = int(episode[1])
           infoList['Title'] = name
           infoList['Plot'] = plot
-          meta = re.compile('<span class="m-episode-meta-item">(.+?)</span>', re.DOTALL).findall(meta)
+          meta = re.compile('<span class="m-episode-meta-item.+?>(.+?)</span>', re.DOTALL).findall(meta)
           if meta is not None:
               duration = 0
               tmp = meta[0].split(':')
@@ -79,7 +76,7 @@ class myAddon(t1mAddon):
               infoList['Date'] = '%s-%s-%s' % ( str(year), mo, day)
               infoList['Aired'] = infoList['Date']
               infoList['Year'] = int(infoList['Aired'].split('-',1)[0])
-              infoList['MPAA'] = meta[2]
+              infoList['MPAA'] = meta[3]
           infoList['TVShowTitle'] = xbmc.getInfoLabel('ListItem.TVShowTitle')
           infoList['Studio'] = 'ABC'
           infoList['mediatype'] = 'episode'
@@ -90,26 +87,25 @@ class myAddon(t1mAddon):
       return(ilist)
 
   def doFunction(self, url):
-    func = url[0:2]
-    url  = url[2:]
-    if func == 'AL':
-      name  = xbmc.getInfoLabel('ListItem.Title')
-      profile = self.addon.getAddonInfo('profile').decode(UTF8)
-      moviesDir  = xbmc.translatePath(os.path.join(profile,'TV Shows'))
-      movieDir  = xbmc.translatePath(os.path.join(moviesDir, name))
-      if not os.path.isdir(movieDir):
-           os.makedirs(movieDir)
-      ilist = []
-      ilist = self.getAddonEpisodes(url, ilist, getFileData = True)
-      for season, episode, url in ilist:
+      func = url[0:2]
+      url  = url[2:]
+      if func == 'AL':
+        name  = xbmc.getInfoLabel('ListItem.Title')
+        profile = self.addon.getAddonInfo('profile').decode(UTF8)
+        moviesDir  = xbmc.translatePath(os.path.join(profile,'TV Shows'))
+        movieDir  = xbmc.translatePath(os.path.join(moviesDir, name))
+        if not os.path.isdir(movieDir):
+            os.makedirs(movieDir)
+        ilist = []
+        ilist = self.getAddonEpisodes(url, ilist, getFileData = True)
+        for season, episode, url in ilist:
             se = 'S%sE%s' % (str(season), str(episode))
             xurl = '%s?mode=GV&url=%s' % (sys.argv[0], url)
             strmFile = xbmc.translatePath(os.path.join(movieDir, se+'.strm'))
             with open(strmFile, 'w') as outfile:
                outfile.write(xurl)         
-    json_cmd = '{"jsonrpc":"2.0","method":"VideoLibrary.Scan", "params": {"directory":"%s/"},"id":1}' % movieDir.replace('\\','/')
-    jsonRespond = xbmc.executeJSONRPC(json_cmd)
-
+      json_cmd = '{"jsonrpc":"2.0","method":"VideoLibrary.Scan", "params": {"directory":"%s/"},"id":1}' % movieDir.replace('\\','/')
+      jsonRespond = xbmc.executeJSONRPC(json_cmd)
 
   def getAddonVideo(self,url):
       vd = uqp(url)
@@ -120,7 +116,7 @@ class myAddon(t1mAddon):
           return
       url = re.compile('<dataUrl>(.+?)</dataUrl>',re.DOTALL).search(html).group(1)
       liz = xbmcgui.ListItem(path = url.strip())
-# No need to process subtitles, all videos have closed captions
+# No need to process subtitles, all shows have closed captions
       infoList ={}
       infoList['Title'] = xbmc.getInfoLabel('ListItem.Title')
       infoList['Year'] = xbmc.getInfoLabel('ListItem.Year')
@@ -133,7 +129,5 @@ class myAddon(t1mAddon):
       infoList['Season'] = xbmc.getInfoLabel('ListItem.Season')
       infoList['Episode'] = xbmc.getInfoLabel('ListItem.Episode')
       liz.setInfo('video', infoList)
-
       xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, liz)
-
 
